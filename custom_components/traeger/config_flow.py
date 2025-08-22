061c4639cb4f4f0ac3a4bdb6957fa5b3add2e859
johnvoipguy <john.voipguy@gmail.com>
2025-08-21 20:04:16 -0700

    Update config_flow.py

--- custom_components/traeger/config_flow.py
+++ custom_components/traeger/config_flow.py
@@ -1,13 +1,13 @@
 """Config flow for Traeger WiFIRE."""
-
 from __future__ import annotations
 
 import logging
 
+import voluptuous as vol
+
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
-import voluptuous as vol
 
 from .const import CONF_PASSWORD, CONF_USERNAME, DOMAIN, PLATFORMS
 from .traeger import traeger
